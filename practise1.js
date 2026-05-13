cfaf139e538b5df7b7e63956ff8ee8ab69549cd4
Mohit raj <80747182+mohitraj4@users.noreply.github.com>
2026-05-13 16:46:58 +0530

    Add files via upload

---
+++ practise1.js
@@ -0,0 +1,38 @@
+// let getData= async()=>{
+//   let apiFirstRes= await fetch(`https://dummyjson.com/products`)
+//   let finalResult=await apiFirstRes.json()
+//   console.log(finalResult.products );
+// }
+// getData();
+
+// let getProducts=()=>{
+//   fetch(`https://dummyjson.com/products`)
+//   .then((res)=>res.json())
+//   .then((finalRes)=>{console.log(finalRes)})
+//   .catch((err)=>{
+//     console.log(`Error fetching products ${err}`);
+
+//   })
+// }
+
+// getProducts();
+//  function getData(){
+//   fetch(`https://dummyjson.com/products`)
+//   .then((res)=>res.json())
+//   .then((finalRes)=>console.log(finalRes))
+
+
+//  }
+
+
+// async function getData(){
+//   setTimeout(()=>{
+//     console.log(`I am inside the seTimeout`);
+
+//   },3000)
+
+// }
+// let obj= getData;
+
+
+  
